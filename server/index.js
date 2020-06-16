9f4a7a341a3c92f8f5e578d1190d0e1e50735f94
kguertin <kevin.m.guertin@gmail.com>
2020-06-16 17:40:02 -0400

    set up socket.io connection

--- server/index.js
+++ server/index.js
@@ -2,11 +2,27 @@ const http = require ('http');
 const express = require('express');
 const socketio = require('socket.io');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
+
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('We have an new connection');
+
+    socket.on('join', ({name, room}) => {
+        console.log(name, room);
+    })
+
+    socket.on('disconnect', () =>{
+        console.log('User has left!!');
+    })
+});
+
+app.use(router);
 
 server.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
